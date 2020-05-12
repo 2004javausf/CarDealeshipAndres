026f372de47158671072e7f850c79f53d83efa4e
Andres Aparicio <andresmijin@gmail.com>
2020-05-12 17:33:08 -0400

    sql statments

---
+++ CarDealer.sql
@@ -0,0 +1,105 @@
+truncate table payments;
+truncate table offers;
+truncate table customer;
+
+drop table car;
+drop table customer;
+drop table offers;
+drop table payments;
+
+create table car(
+car_id number (20),
+car_brand varchar2(20),
+car_mileage float,
+car_year integer,
+car_price float,
+car_isbought integer);
+
+ALTER TABLE car
+  ADD (
+    CONSTRAINT car_pk PRIMARY KEY (id)
+  );
+
+CREATE SEQUENCE car_sequence;
+
+CREATE OR REPLACE TRIGGER cars_on_insert
+  BEFORE INSERT ON car
+  FOR EACH ROW
+BEGIN
+  SELECT car_sequence.nextval
+  INTO :new.id
+  FROM dual;
+END;
+
+create table customer(
+customer_id number (20),
+customer_name varchar2(20),
+customer_last_name varchar2(20)
+);
+
+ALTER TABLE customer
+  ADD (
+    CONSTRAINT customer_pk PRIMARY KEY (id)
+  );
+
+CREATE SEQUENCE customer_sequence;
+
+CREATE OR REPLACE TRIGGER customer_on_insert
+  BEFORE INSERT ON customer
+  FOR EACH ROW
+BEGIN
+  SELECT customer_sequence.nextval
+  INTO :new.id
+  FROM dual;
+END;
+
+create table offers(
+offer_id number (20),
+customer_id number(20),
+car_id number(20),
+car_price float,
+car_price_offer float,
+offer_status varchar2
+);
+
+ALTER TABLE offers
+  ADD (
+    CONSTRAINT offersr_pk PRIMARY KEY (id)
+  );
+
+CREATE SEQUENCE offers_sequence;
+
+CREATE OR REPLACE TRIGGER offers_on_insert
+  BEFORE INSERT ON offers
+  FOR EACH ROW
+BEGIN
+  SELECT offers_sequence.nextval
+  INTO :new.id
+  FROM dual;
+END;
+
+create table payments(
+payment_id number (20),
+car_id number(20),
+customer_id number(20),
+car_price float,
+car_payment float,
+car_balance varchar2
+);
+
+ALTER TABLE payments
+  ADD (
+    CONSTRAINT payments_pk PRIMARY KEY (id)
+  );
+
+CREATE SEQUENCE payments_sequence;
+
+CREATE OR REPLACE TRIGGER payments_on_insert
+  BEFORE INSERT ON payments
+  FOR EACH ROW
+BEGIN
+  SELECT payments_sequence.nextval
+  INTO :new.id
+  FROM dual;
+END;
+
